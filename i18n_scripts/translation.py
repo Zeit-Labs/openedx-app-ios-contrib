b974deac85a5441b1617561da2a56af04de6d246
Omar Al-Ithawi <i@omardo.com>
2024-06-25 23:20:40 +0300

    docs: add code docs

--- i18n_scripts/translation.py
+++ i18n_scripts/translation.py
@@ -44,6 +44,9 @@ def parse_arguments():
 
 
 def get_modules_dir(override: Path = None) -> Path:
+    """
+    Gets the modeles directory (repository root directory).
+    """
     if override:
         return override
 
@@ -278,18 +281,29 @@ def split_translation_files(modules_dir=None):
 
 
 def get_xcode_project(modules_dir: Path, module_name: str) -> XcodeProject:
+    """
+    Initialize an XCode project instance for a given module.
+    """
     module_path = modules_dir / module_name
     xcode_project = XcodeProject.load(module_path / f'{module_name}.xcodeproj/project.pbxproj')
     return xcode_project
 
 
 def list_translation_files(module_path: Path) -> [Path]:
+    """
+    List translaiton files in a given path.
+
+    This method doesn't return the `en.lproj` translation source strings.
+    """
     for localizable_abs_path in module_path.rglob('**/Localizable.strings'):
         if localizable_abs_path.parent.name != 'en.lproj':
             yield localizable_abs_path
 
 
 def get_xcode_projects(modules_dir: Path) -> [{Path, XcodeProject}]:
+    """
+    Return a list of module_name, xcode_project pairs.
+    """
     for module_name in get_modules_to_translate(str(modules_dir)):
         print(f'## Entering project: {module_name}')
         xcode_project = get_xcode_project(modules_dir, module_name)
@@ -297,6 +311,9 @@ def get_xcode_projects(modules_dir: Path) -> [{Path, XcodeProject}]:
 
 
 def add_translation_files_to_xcode(modules_dir: Path = None):
+    """
+    Add Localizable.strings files pulled from Transifex to XCode.
+    """
     try:
         modules_dir = get_modules_dir(override=modules_dir)
 
@@ -315,6 +332,9 @@ def add_translation_files_to_xcode(modules_dir: Path = None):
 
 
 def clean_translation_files(modules_dir: Path = None):
+    """
+    Remove translation files from both file system and XCode project files.
+    """
     try:
         modules_dir = get_modules_dir(override=modules_dir)
 
@@ -334,7 +354,6 @@ def clean_translation_files(modules_dir: Path = None):
                     xcode_project.remove_files_by_path(file_ref.path, tree=LOCALIZABLE_FILES_TREE)
 
             xcode_project.save()
-
     except Exception as e:
         print(f"Error: An unexpected error occurred in clean_translation_files: {e}", file=sys.stderr)
         raise
